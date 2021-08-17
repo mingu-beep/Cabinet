a1eaaad9b98463839d1375d476e3e054020ca6d3
mingu-beep <bramurve@gmail.com>
2021-08-17 12:32:17 +0900

    Setup

---
+++ src/test/java/com/cabinet/Cabinet/CabinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabinet.Cabinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
