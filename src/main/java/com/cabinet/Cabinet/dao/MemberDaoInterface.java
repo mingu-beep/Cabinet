43f5d21c0b2e717aa363db564bd5b4d609254098
elody2 <89057547+elody2@users.noreply.github.com>
2021-08-18 22:33:22 +0900

    회원가입 넘어가유~

--- src/main/java/com/cabinet/Cabinet/dao/MemberDaoInterface.java
+++ src/main/java/com/cabinet/Cabinet/dao/MemberDaoInterface.java
@@ -10,8 +10,8 @@ public interface MemberDaoInterface {
 	
 	//유저정보
 	int regMember(MemberDTO memberDTO) throws SQLException; //유저 회원가입 메서드
-	int getKey(String inputId, String member_key); //유저 인증키 생성 메서드
-	int alter_memberKey(String inputId, String key); //유저 인증키 y로 바꿔주는 메서드
-	int searchPassword(String inputId, String inputEmail, String key); //회원 임시 비밀번호 변경 메서드
-	MemberDTO loginMember(@Param("inputId")String inputId); //유저 로그인 메서드
+	int getKey(String memID, String member_key); //유저 인증키 생성 메서드
+	int alter_memberKey(String memID, String key); //유저 인증키 y로 바꿔주는 메서드
+	int searchPassword(String memID, String memEmail, String key); //회원 임시 비밀번호 변경 메서드
+	MemberDTO loginMember(@Param("memID")String memID); //유저 로그인 메서드
 }
--- src/main/java/com/cabinet/Cabinet/dto/MemberDTO.java
+++ src/main/java/com/cabinet/Cabinet/dto/MemberDTO.java
@@ -14,5 +14,48 @@ public class MemberDTO {
     private String memPhone;
     private String memAdrs;
     private String memBday;
+    
+	public String getMemID() {
+		return memID;
+	}
+	public void setMemID(String memID) {
+		this.memID = memID;
+	}
+	public String getMemPw() {
+		return memPw;
+	}
+	public void setMemPw(String memPw) {
+		this.memPw = memPw;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	public String getMemEmail() {
+		return memEmail;
+	}
+	public void setMemEmail(String memEmail) {
+		this.memEmail = memEmail;
+	}
+	public String getMemPhone() {
+		return memPhone;
+	}
+	public void setMemPhone(String memPhone) {
+		this.memPhone = memPhone;
+	}
+	public String getMemAdrs() {
+		return memAdrs;
+	}
+	public void setMemAdrs(String memAdrs) {
+		this.memAdrs = memAdrs;
+	}
+	public String getMemBday() {
+		return memBday;
+	}
+	public void setMemBday(String memBday) {
+		this.memBday = memBday;
+	}
 
 }
