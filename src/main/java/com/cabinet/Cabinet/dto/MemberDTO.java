3f04e0cab2ac297c3af12082c2c66ffdeca77c0e
mingu-beep <bramurve@gmail.com>
2021-08-18 16:42:36 +0900

    member database 컬럼명 변경

--- src/main/java/com/cabinet/Cabinet/dto/MemberDTO.java
+++ src/main/java/com/cabinet/Cabinet/dto/MemberDTO.java
@@ -1,77 +1,18 @@
 package com.cabinet.Cabinet.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MemberDTO {
 
-    private String inputId;
-    private String inputPassword;
-    private String inputName;
-    private String inputBDay;
-    private String gender;
-    private String inputEmail;
-    private String inputPhone;
-    private String inputAddress;
+    private String memID;
+    private String memPw;
+    private String memName;
+    private String memEmail;
+    private String memPhone;
+    private String memAdrs;
+    private String memBday;
 
-    public String getInputId() {
-        return inputId;
-    }
-
-    public void setInputId(String inputId) {
-        this.inputId = inputId;
-    }
-
-    public String getInputPassword() {
-        return inputPassword;
-    }
-
-    public void setInputPassword(String inputPassword) {
-        this.inputPassword = inputPassword;
-    }
-
-    public String getInputName() {
-        return inputName;
-    }
-
-    public void setInputName(String inputName) {
-        this.inputName = inputName;
-    }
-
-    public String getInputBDay() {
-        return inputBDay;
-    }
-
-    public void setInputBDay(String inputBDay) {
-        this.inputBDay = inputBDay;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getInputEmail() {
-        return inputEmail;
-    }
-
-    public void setInputEmail(String inputEmail) {
-        this.inputEmail = inputEmail;
-    }
-
-    public String getInputPhone() {
-        return inputPhone;
-    }
-
-    public void setInputPhone(String inputPhone) {
-        this.inputPhone = inputPhone;
-    }
-
-    public String getInputAddress() {
-        return inputAddress;
-    }
-
-    public void setInputAddress(String inputAddress) {
-        this.inputAddress = inputAddress;
-    }
 }
