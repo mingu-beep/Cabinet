84004fc4a5e5b1bc3a8c7a5ce95b185406e3c25c
elody2 <89057547+elody2@users.noreply.github.com>
2021-08-18 11:59:15 +0900

    회원가입 추가

--- src/main/java/com/cabinet/Cabinet/controller/MemberController.java
+++ src/main/java/com/cabinet/Cabinet/controller/MemberController.java
@@ -1,18 +1,42 @@
 package com.cabinet.Cabinet.controller;
 
+import java.lang.reflect.Member;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.cabinet.Cabinet.dto.MemberDTO;
+import com.cabinet.Cabinet.service.MemberService;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
+
+	@Autowired
+	private MemberService member_service;
+//	@Autowired
+//	private AdminStoreListService store_service;
+//	@Autowired
+//	private UserMailSendService mail_sender;
 	
-	@GetMapping("/join")
-	public String memberJoin(Model model) {
-		return "join";
+	//회원가입 컨트롤러
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
+	public String memberPass(MemberDTO memberDTO) {
+		//회원가입 메서드
+		member_service.member_service(memberDTO);
+		//인증 메일 보내기 메서드
+//		mail_sender.mailSendWithUserKey(memberDTO.getMem_email(), memberDTO.getMem_id());
+		
+		return "redirect:/";
 	}
+//	@GetMapping("/join")
+//	public String memberJoin(Model model) {
+//		return "join";
+//	}
 	@GetMapping("/login")
 	public String memberLogin(Model model) {
 		return "login";
---
+++ src/main/java/com/cabinet/Cabinet/service/MemberService.java
@@ -0,0 +1,31 @@
+package com.cabinet.Cabinet.service;
+
+import java.sql.SQLException;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cabinet.Cabinet.dao.MemberDaoInterface;
+import com.cabinet.Cabinet.dto.MemberDTO;
+
+@Service
+public class MemberService {
+	@Autowired
+	private SqlSessionTemplate memberSqlSession;
+	private MemberDaoInterface memberDao;
+	
+	//회원가입 서비스
+	public int member_service(MemberDTO memberDTO) {
+		int resultCnt = 0;
+		
+		memberDao = memberSqlSession.getMapper(MemberDaoInterface.class);
+		try {
+			resultCnt = memberDao.regMember(memberDao);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return resultCnt;
+	}
+}
